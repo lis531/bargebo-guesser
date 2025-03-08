3a9b664a1d5c390de41d5e17193761a2c1d6ebe4
lis531 <82408934+lis531@users.noreply.github.com>
2025-03-08 21:06:04 +0100

    Update
    Songs are now chosen on server side, downloading song on server side

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,12 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
+import ytSearch from 'yt-search';
+import { exec } from 'child_process';
+import fs from 'fs';
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
@@ -13,6 +17,87 @@ const io = new Server(server, {
 });
 
 const lobbies = {};
+
+async function getAccessToken() {
+    const clientId = 'b8156c11c6ca4c32b541d3392225aed3';
+    const clientSecret = '24d6189fba74450fb8a7917fa150fcea';
+
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: `Basic ${Buffer.from(clientId + ':' + clientSecret).toString('base64')}`,
+        },
+        body: 'grant_type=client_credentials',
+    });
+
+    const data = await response.json();
+    return data.access_token;
+}
+
+async function fetchWebApi(endpoint, method, body) {
+    const token = await getAccessToken();
+    const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        method,
+        body: body ? JSON.stringify(body) : undefined,
+    });
+
+    return await res.json();
+}
+
+async function getPopularTracks() {
+    const data = await fetchWebApi('v1/search?q=track&type=track&limit=50', 'GET');
+    const popularTracks = data.tracks.items.filter((track) => track.popularity > 50);
+    return popularTracks;
+}
+
+const fetchTracks = async () => {
+    const popularTracks = await getPopularTracks();
+    if (popularTracks.length === 0) {
+        console.log('No tracks found with popularity > 50.');
+        return [];
+    }
+
+    const selectedTracks = [];
+    for (let i = 0; i < 4; i++) {
+        const randomTrack = popularTracks[Math.floor(Math.random() * popularTracks.length)];
+        selectedTracks.push(randomTrack);
+    }
+
+    const songs = selectedTracks.map(track => ({
+        id: track.id,
+        title: track.name,
+        artist: track.artists.map(artist => artist.name).join(', '),
+        cover: track.album.images[0]?.url || '',
+        url: track.external_urls.spotify,
+    }));
+    return songs;
+};
+
+async function downloadSong(videoUrl) {
+    fs.unlink('downloadedSong.mp3', (err) => {
+        if (err && err.code !== 'ENOENT') {
+            console.error(`Error removing file: ${err.message}`);
+        } else {
+            console.log('File removed');
+        }
+    });
+    return new Promise((resolve, reject) => {
+        exec(`yt-dlp -x --audio-format mp3 -o "downloadedSong.%(ext)s" ${videoUrl}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error downloading track: ${error.message}`);
+                reject(error);
+            } else {
+                console.log(`Track downloaded: ${stdout}`);
+                resolve('downloadedSong.mp3');
+            }
+        });
+    });
+}
 
 io.on('connection', (socket) => {
     console.log(`Client connected: ${socket.id}`);
@@ -24,12 +109,14 @@ io.on('connection', (socket) => {
             socket.emit('createLobbyResponse', lobbyName, 'Lobby already exists!');
             return;
         }
-        
-        lobbies[lobbyName] = { players: [{ id: socket.id, name: username, choice: -1 }], roundStarted: false };
+
+        lobbies[lobbyName] = {
+            players: [{ id: socket.id, name: username, choice: -1 }],
+            roundStarted: false
+        };
         socket.join(lobbyName);
-        
+
         socket.emit('createLobbyResponse', lobbyName, '');
-        
         io.emit('onLobbyListChanged', Object.keys(lobbies));
         io.to(lobbyName).emit('onPlayersChanged', lobbies[lobbyName].players);
 
@@ -42,12 +129,12 @@ io.on('connection', (socket) => {
             return;
         }
 
-        for (var i = 0; i < lobbies[lobbyName].players.length; i++) {
-            if (lobbies[lobbyName].players[i].name === username) {
+        for (const player of lobbies[lobbyName].players) {
+            if (player.name === username) {
                 socket.emit('joinLobbyResponse', 'Player with this name already is in the lobby!');
                 return;
             }
-            if (lobbies[lobbyName].players[i].id === socket.id) {
+            if (player.id === socket.id) {
                 socket.emit('joinLobbyResponse', 'This client is already connected to the lobby!');
                 return;
             }
@@ -57,51 +144,58 @@ io.on('connection', (socket) => {
         socket.join(lobbyName);
 
         socket.emit('joinLobbyResponse', '');
-
         io.to(lobbyName).emit('onPlayersChanged', lobbies[lobbyName].players);
 
         console.log(`User ${username} joined lobby: ${lobbyName}`);
     });
 
-    socket.on('announceRoundStart', (lobbyName) => {
-        for (var i = 0; i < lobbies[lobbyName].players.length; i++) {
-            lobbies[lobbyName].players[i].choice = -1;
+    socket.on('announceRoundStart', async (lobbyName) => {
+        if (!lobbies[lobbyName]) return;
+
+        for (const player of lobbies[lobbyName].players) {
+            player.choice = -1;
         }
 
         lobbies[lobbyName].roundStarted = true;
 
-        const allSongs = ['A', 'B', 'C', 'D']
+        const allSongs = await fetchTracks();
         const correctIndex = 1;
 
+        console.log();
+
+        const query = allSongs[correctIndex].title + " " + allSongs[correctIndex].artist;
+        const searchResults = await ytSearch(query);
+        const videoUrl = searchResults.videos[0].url;
+        io.to(lobbyName).emit('onSongDownloaded', await downloadSong(videoUrl));
         io.to(lobbyName).emit('onRoundStart', allSongs, correctIndex);
     });
+
+    socket.on('announceRoundEnd', (lobbyName) => {
+        if (!lobbies[lobbyName]) return;
+        lobbies[lobbyName].roundStarted = false;
+        io.to(lobbyName).emit('onRoundEnd');
+    });
+
     socket.on('submitAnswer', (lobbyName, choiceIndex) => {
-        if (lobbies[lobbyName].roundStarted === false) {
-            console.log(socket.id, ' was late...');
+        if (!lobbies[lobbyName] || lobbies[lobbyName].roundStarted === false) {
+            console.log(`${socket.id} submitted answer too late or lobby doesn't exist.`);
             return;
         }
 
-        for (var i = 0; i < lobbies[lobbyName].players.length; i++) {
-            if (lobbies[lobbyName].players[i].id === socket.id) {
-                lobbies[lobbyName].players[i].choice = choiceIndex;
-                return;
+        for (const player of lobbies[lobbyName].players) {
+            if (player.id === socket.id) {
+                player.choice = choiceIndex;
+                break;
             }
         }
-    });
-    socket.on('announceRoundEnd', (lobbyName) => {
-        lobbies[lobbyName].roundStarted = false;
-
-        io.to(lobbyName).emit('onRoundEnd');
     });
 
     socket.on('disconnect', () => {
         for (const lobby in lobbies) {
             lobbies[lobby].players = lobbies[lobby].players.filter(p => p.id !== socket.id);
-
             io.to(lobby).emit('onPlayersChanged', lobbies[lobby].players);
-            if (lobbies[lobby].players.length === 0) { 
+            if (lobbies[lobby].players.length === 0) {
                 delete lobbies[lobby];
-
                 io.emit('onLobbyListChanged', Object.keys(lobbies));
             }
         }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,8 @@ import './App.css';
 import SongPicker from './SongPicker.tsx';
 import { io } from "socket.io-client";
 
-const socket = io("http://130.162.248.218:2137");
+// const socket = io("http://130.162.248.218:2137");
+const socket = io("http://localhost:2137");
 
 function App() {
 	const [lobbyName, setLobbyName] = useState<string>("");
@@ -12,25 +13,17 @@ function App() {
 	const [lobbyNames, setLobbyNames] = useState([]);
 	const [currentLobby, setCurrentLobby] = useState("");
 	const [selectedSong, setSelectedSong] = useState<{ id: string; title: string; artist: string } | null>(null);
-	const [songs, setSongs] = useState<{ id: string; title: string; artist: string; cover: string; url: string; }[]>([]);	
+	const [songs, setSongs] = useState<{ id: string; title: string; artist: string; cover: string; url: string; }[]>([]);
 
 	useEffect(() => {
 		socket.on('onLobbyListChanged', (lobbyNames) => {
 			setLobbyNames(lobbyNames);
-
 			console.log("Lobby names changed: ", lobbyNames);
 		});
+
 		socket.on('onPlayersChanged', (players) => {
 			setLobbyPlayers(players);
-
 			console.log("Players changed: ", players);
-		});
-
-		socket.on('onRoundStart', (allSongs, correctIndex) => {
-			console.log("Starting round: ", allSongs, " correct: ", correctIndex);
-		});
-		socket.on('onRoundEnd', () => {
-			console.log("Ending round.");
 		});
 	
 		socket.on("createLobbyResponse", (lobbyName, err) => {
@@ -50,6 +43,20 @@ function App() {
 
 			console.log("Successfully joined a lobby called: ", lobbyName);
 		});
+
+		socket.on('onRoundStart', (allSongs, correctIndex) => {
+			console.log("Starting round: ", allSongs, " correct index: ", correctIndex);
+			setSongs(allSongs);
+		});
+
+		socket.on('onRoundEnd', () => {
+			console.log("Ending round.");
+		});
+
+		socket.on("onSongDownloaded", (correctSong) => {
+			const audio = new Audio(correctSong);
+			audio.play();
+		});
 	}, [])
 
 	const createLobby = () => {
@@ -64,14 +71,6 @@ function App() {
 		}
 	};
 
-	const startRound = () => {
-		socket.emit('announceRoundStart', lobbyName);
-	}
-
-	const submitAnswer = (choiceIndex: number) => {
-		socket.emit('submitAnswer', lobbyName, choiceIndex);
-	}
-
 	useEffect(() => {
 		const handleKeyDown = (e: KeyboardEvent) => {
 			if (e.key === " ") {
@@ -85,106 +84,21 @@ function App() {
 		};
 	}, [lobbyNames]);
 
-	interface Artist {
-		name: string;
+	const startRound = () => {
+		socket.emit('announceRoundStart', lobbyName);
 	}
 
-	interface Track {
-		album: any;
-		name: string;
-		artists: Artist[];
-		popularity: number;
-		url: string;
+	const endRound = () => {
+		socket.emit('announceRoundEnd', lobbyName);
 	}
 
-	async function getAccessToken() {
-		const clientId = 'b8156c11c6ca4c32b541d3392225aed3';
-		const clientSecret = '24d6189fba74450fb8a7917fa150fcea';
-
-		const response = await fetch('https://accounts.spotify.com/api/token', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-				Authorization: `Basic ${btoa(clientId + ':' + clientSecret)}`,
-			},
-			body: 'grant_type=client_credentials',
-		});
-
-		const data = await response.json();
-		return data.access_token;
+	const submitAnswer = (choiceIndex: number) => {
+		socket.emit('submitAnswer', lobbyName, choiceIndex);
 	}
-
-	async function fetchWebApi(endpoint: string, method: string, body?: any) {
-		const token = await getAccessToken();
-		const res = await fetch(`https://api.spotify.com/${endpoint}`, {
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-			method,
-			body: body ? JSON.stringify(body) : undefined,
-		});
-
-		return await res.json();
-	}
-
-
-	async function getPopularTracks(): Promise<Track[]> {
-		const data = await fetchWebApi(
-			'v1/search?q=track&type=track&limit=50', 'GET', undefined
-		);
-
-		const popularTracks = data.tracks.items.filter((track: Track) => track.popularity > 50);
-
-		return popularTracks;
-	}
-
-	useEffect(() => {
-		const fetchTracks = async () => {
-			const popularTracks = await getPopularTracks();
-
-			if (popularTracks.length === 0) {
-				console.log('No tracks found with popularity > 50.');
-				return;
-			}
-
-			const selectedTracks = [];
-			for (let i = 0; i < 4; i++) {
-				const randomTrack = popularTracks[Math.floor(Math.random() * popularTracks.length)];
-				selectedTracks.push(randomTrack);
-			}
-
-			setSongs(selectedTracks.map(track => ({
-				id: track.name,
-				title: track.name,
-				artist: track.artists.map(artist => artist.name).join(', '),
-				cover: track.album.images[0].url,
-				url: track.url,
-			})));
-		};
-
-		fetchTracks();
-	}, []);
 
 	const onSongSelection = (song: { id: string; title: string; artist: string; url: string }) => {
 		setSelectedSong(song);
-		
 	};
-
-	const playSong = () => {
-		if (songs.length === 0) {
-			console.log("No songs available to play.");
-			return;
-		}
-	
-		const randomIndex = Math.floor(Math.random() * songs.length);
-		const randomSong = songs[randomIndex];
-	
-		const audio = new Audio(randomSong.url);
-		audio.play();
-	
-		console.log(`Playing: ${randomSong.title} by ${randomSong.artist}`);
-	};
-	
 
 	return (
 		<div className="start-screen">
@@ -198,9 +112,9 @@ function App() {
 					<button type="submit" onClick={() => joinLobby()}>Join Lobby</button>
 					<button type="submit" onClick={() => createLobby()}>Create Lobby</button>
 					<button type="submit" onClick={() => startRound()}>Start Round</button>
+					<button type="submit" onClick={() => endRound()}>End Round</button>
 				</div>
 			</div>
-			<button onClick={() => playSong()}>Play Song</button>
 			<SongPicker songs={songs} onSongSelect={onSongSelection} />
 		</div>
 	);
