6964d2cb5511c405c0454b3397866877df7f6430
lis531 <82408934+lis531@users.noreply.github.com>
2025-03-15 18:26:15 +0100

    Environment Variables
    Moved all API keys to .env

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,27 @@ import { Server } from 'socket.io';
 import ytSearch from 'yt-search';
 import { exec } from 'child_process';
 import fs from 'fs';
-import LAST_FM_API_KEY from './secrets.js';
 
 import admin from 'firebase-admin';
+import dotenv from 'dotenv';
 
-const serviceAccount = JSON.parse(fs.readFileSync('firebase-admin-key.json', 'utf8'));
+dotenv.config();
+
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
+
+const LAST_FM_API_KEY = process.env.LAST_FM_API_KEY;
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
